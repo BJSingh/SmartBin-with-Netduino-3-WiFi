b986a91f4d6062b596f07cfcd10b05b560f70a1f
Brijesh Singh <brijeshs927@gmail.com>
2018-08-24 11:30:55 +0530

    Create Blinky.cs

---
+++ Blinky.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Blinky
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // create an output port (a port that can be written to) and wire it to the onboard LED
+            OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+
+            // run forever
+            while (true)
+            {
+                led.Write(true); // turn on the LED
+                Thread.Sleep(250); // sleep for 250ms
+                led.Write(false); // turn off the LED
+                Thread.Sleep(250); // sleep for 250ms
+            }
+        }
+    }
+}
